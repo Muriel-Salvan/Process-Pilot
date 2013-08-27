70af1774fc5a30c8f9b078e3382762e0df958b95
Muriel Salvan <Muriel@X-Aeon.com>
2013-08-27 15:27:02 +0200

    Removed useless Copyrights

--- lib/processpilot/io.rb
+++ lib/processpilot/io.rb
@@ -1,8 +1,3 @@
-#--
-# Copyright (c) 2012 Muriel Salvan (muriel@x-aeon.com)
-# Licensed under the terms specified in LICENSE file. No warranty is provided.
-#++
-
 # Define some helpers that can be handy in case of process piloting from IO
 class IO
 
--- test/Programs/Ruby/NormalSTDOUT.rb
+++ test/Programs/Ruby/NormalSTDOUT.rb
@@ -1,8 +1,3 @@
-#--
-# Copyright (c) 2012 Muriel Salvan (muriel@x-aeon.com)
-# Licensed under the terms specified in LICENSE file. No warranty is provided.
-#++
-
 require "#{File.dirname(__FILE__)}/Scenario.rb"
 
 ProcessPilotTest::Scenario::run
